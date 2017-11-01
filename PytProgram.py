593ec7dfe3eb3e38bd6f0eba30a9cc812591342b
Unknown <jackjramey@gmail.com>
2017-10-31 20:26:47 -0700

    te

---
+++ PytProgram.py
@@ -0,0 +1,22 @@
+#
+# Python Program
+#
+#
+print("Decimal Calculator")
+a = input("Enter First Number: ")
+a = float(a)
+b = input("Enter Second Number: ")
+b = float(b)
+x = int(input("Now chose your operator 1)/ 2)* 3)- 4)+: "))
+
+
+if x == 1:
+    print(float(a / b))
+elif x == 2:
+    print(float(a * b))
+elif x == 3:
+    print(float(a - b))
+elif x == 4:
+    print(float(a - b))
+else:
+    print("Error")
