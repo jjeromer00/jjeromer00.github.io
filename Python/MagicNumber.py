73d6ed512cc20e24e280cf60121a1161ec347cb4
Unknown <jackjramey@gmail.com>
2017-11-15 11:26:13 -0800

    fix

---
+++ Python/MagicNumber.py
@@ -0,0 +1,24 @@
+#MagicNumber
+#
+#
+#
+print('Magic Number 3')
+print('In put any number and it will be Turned to the Number 3')
+x = input('Input number here: ')
+x = int(x)
+y = x + 5
+int(y)
+l = input('Now Add 5 to your number(Hit enter to continue): ')
+print(y)
+z = y * 2
+m = input('Now Double the result(Hit enter to continue): ')
+print(z)
+a = z - 4
+n = input('Now Subtract 4 from the result(Hit enter to continue): ')
+print(a)
+b = a / 2
+o = input('Divide the result by 2(Hit enter to continue): ')
+print(b)
+c = b - x
+p = input('Finally subtract your original number(Hit enter to continue): ')
+print(c)
