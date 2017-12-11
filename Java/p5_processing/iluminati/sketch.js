0b9281b6ec89615b5f21d25bdc30ec7c72d54190
Unknown <jackjramey@gmail.com>
2017-12-10 16:48:11 -0800

    a

--- Java/p5_processing/iluminati/sketch.js
+++ Java/p5_processing/iluminati/sketch.js
@@ -19,7 +19,7 @@ function windowResized() {
 function draw() {
   fill(130,140,200, 100);
 
-  for (let x = 155; x <= 200; x += 5) {
+  for (let x = 10; x <= 200; x += 10) {
     fill(x + 30, x * 17, x * 10, 100);
     rect(x, x + 5, x * 5, 110);
   }
